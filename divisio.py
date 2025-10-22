f05bf677d4c1097795407f23d91eb077fa6528c4
Gabriela <gpaulini@instituticaria.cat>
2025-10-22 10:26:55 +0200

    :)

---
+++ divisio.py
@@ -0,0 +1,7 @@
+A=float(input("Divisor de la operación"))
+B=float(input("Dividendo de la operación"))
+
+Cociente=B//A
+Resto=B%A
+
+print(f"División: {B/A} /n", f"Cociente:{Cociente} /n", f"Resto:{Resto}")

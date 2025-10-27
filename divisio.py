f5ba7aca72cd7bd41ec9e020148b0a6f38a193c3
Gabriela <gpaulini@instituticaria.cat>
2025-10-27 08:18:51 +0100

    correción

--- divisio.py
+++ divisio.py
@@ -1,7 +1,7 @@
-A=int(input("Divisor de la operación"))
-B=int(input("Dividendo de la operación"))
+B = int(input("Dividendo de la operación"))
+A = int(input("Divisor de la operación"))
 
-Cociente=B//A
-Resto=B%A
+Cociente = B//A
+Resto = B % A
 
 print(f"División: {B/A} /n", f"Cociente:{Cociente} /n", f"Resto:{Resto}")
